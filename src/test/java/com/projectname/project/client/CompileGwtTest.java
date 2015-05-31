ec30f421c09a6403207ea0ff118f38adeef7bca0
Ivan Kyryluk <ivankyrios@gmail.com>
2015-05-31 21:30:21 +0300

    GWT App skeleton

---
+++ src/test/java/com/projectname/project/client/CompileGwtTest.java
@@ -0,0 +1,16 @@
+package com.projectname.project.client;
+
+import com.google.gwt.junit.client.GWTTestCase;
+
+public class CompileGwtTest extends GWTTestCase {
+  
+  @Override
+  public String getModuleName() {
+    return "com.projectname.project.VKPlistSaver";
+  }
+
+  public void testSandbox() {
+    assertTrue(true);
+  }
+  
+}
